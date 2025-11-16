6072821b2ae1499d50f1aef0290798ce4fd4dafc
Venugopal Reddy <venugopalreddy1895@gmail.com>
2025-11-16 18:30:52 +0530

    styleing the homepage

--- frontend/src/landing_page/Navbar.js
+++ frontend/src/landing_page/Navbar.js
@@ -1,6 +1,59 @@
-import React from 'react';
+import React from "react";
 function Navbar() {
-    return ( <h1>Navbar</h1> );
+  return (
+    
+      <nav class="navbar navbar-expand-lg border-bottom sticky-top " style={{backgroundColor: "#FFF"}}>
+        <div class="container ">
+          <a class="navbar-brand" href="#">
+            <img src="media/logo.svg" style={{width:"25%"}}></img>
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+           
+            <form class="d-flex" role="search">
+             <ul class="navbar-nav  mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                  Signup
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="#">
+                  About
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="#">
+                  Products
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="#">
+                  Pricin
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="#">
+                  Support
+                </a>
+              </li>
+            </ul>
+            </form>
+          </div>
+        </div>
+      </nav>
+    
+  );
 }
 
 export default Navbar;
